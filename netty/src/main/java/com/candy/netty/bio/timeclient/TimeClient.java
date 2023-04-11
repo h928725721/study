6f6d9611bf2463f7d9bccbe8d555c130fc8866cf
JOKER <928725721@qq.com>
2023-04-11 21:45:49 +0800

    feat:netty模块新增TimeServer测试代码

---
+++ netty/src/main/java/com/candy/netty/bio/timeclient/TimeClient.java
@@ -0,0 +1,59 @@
+package com.candy.netty.bio.timeclient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TimeClient {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                //do something
+            }
+        }
+
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            socket = new Socket("127.0.0.1", port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(),true);
+            //通过输出流向服务端发送指令
+            out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed");
+            String resp = in.readLine();
+            System.out.println("Now is : " + resp);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (out != null) {
+                out.close();
+                out = null;
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                in = null;
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                socket = null;
+            }
+        }
+
+
+    }
+}
