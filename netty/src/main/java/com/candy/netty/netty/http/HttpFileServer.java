cd3f55733892cdaf4e090a7626aad56779d03f22
戚中校 <qizhongxiao@yeetech.com>
2023-04-27 16:13:26 +0800

    netty http

--- netty/src/main/java/com/candy/netty/netty/http/HttpFileServer.java
+++ netty/src/main/java/com/candy/netty/netty/http/HttpFileServer.java
@@ -1,4 +1,68 @@
 package com.candy.netty.netty.http;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
 public class HttpFileServer {
+
+    private static final String DEFAULT_URL = "/netty/src/main/java/com/candy/netty";
+
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 8080;
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                //采用默认值
+            }
+        }
+        String url = DEFAULT_URL;
+        if (args.length > 1) {
+            url = args[1];
+        }
+        new HttpFileServer().run(port, url);
+    }
+
+    public void run(final int port, final String url) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                            //将多个消息转换为单一得FullHttpRequest或者FullHttpResponse
+                            ch.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                            ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                            ch.pipeline().addLast("fileServerHandler", new HttpFileServerHandler(url));
+                        }
+                    });
+
+            // Start the server.
+            ChannelFuture f = b.bind("127.0.0.1", port).sync();
+            System.out.println("HTTP 文件目录服务器启动，网址是 ： " + "http://127.0.0.1:" + port + url);
+            // Wait until the server socket is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            // Shut down all event loops to terminate all threads.
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
 }
--- netty/src/main/java/com/candy/netty/netty/http/HttpFileServerHandler.java
+++ netty/src/main/java/com/candy/netty/netty/http/HttpFileServerHandler.java
@@ -1,11 +1,186 @@
 package com.candy.netty.netty.http;
 
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http2.HttpUtil;
+import io.netty.handler.stream.ChunkedFile;
+import io.netty.util.CharsetUtil;
+import io.netty.handler.codec.http.HttpHeaders;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
 
 public class HttpFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
-    @Override
-    protected void messageReceived(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
+
+    private final String url;
+
+    public HttpFileServerHandler(String url) {
+        this.url = url;
     }
+
+
+    @Override
+    protected void messageReceived(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception {
+
+        if (!req.decoderResult().isSuccess()) {
+            sendError(ctx, HttpResponseStatus.BAD_REQUEST);
+            return;
+        }
+        if (req.method() != HttpMethod.GET) {
+            sendError(ctx, HttpResponseStatus.METHOD_NOT_ALLOWED);
+            return;
+        }
+        final String uri = req.uri();
+        final String path = sanitizeUri(uri);
+        if (path == null) {
+            sendError(ctx, HttpResponseStatus.FORBIDDEN);
+            return;
+        }
+        File file = new File(path);
+        if (file.isHidden() || !file.exists()) {
+            sendError(ctx, HttpResponseStatus.NOT_FOUND);
+            return;
+        }
+        if (file.isDirectory()) {
+            if (uri.endsWith("/")) {
+                sendListing(ctx, file);
+            } else {
+                sendRedirect(ctx, uri + '/');
+            }
+            return;
+        }
+        if (!file.isFile()) {
+            sendError(ctx, HttpResponseStatus.FORBIDDEN);
+            return;
+        }
+        RandomAccessFile raf;
+        try {
+            raf = new RandomAccessFile(file, "r");
+        } catch (FileNotFoundException ignore) {
+            sendError(ctx, HttpResponseStatus.NOT_FOUND);
+            return;
+        }
+        long fileLength = raf.length();
+        HttpResponse response = new DefaultHttpResponse(req.protocolVersion(), HttpResponseStatus.OK);
+        response.headers().setInt(HttpHeaderNames.CONTENT_LENGTH, (int) fileLength);
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/octet-stream");
+        boolean keepAlive = HttpHeaderValues.KEEP_ALIVE.equals(req.headers().get(HttpHeaderNames.CONNECTION));
+        if (keepAlive) {
+            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+        }
+        ctx.write(response);
+        ChannelFuture sendFileFuture;
+        sendFileFuture = ctx.write(new HttpChunkedInput(new ChunkedFile(raf, 0, fileLength, 8192)),
+                ctx.newProgressivePromise());
+        sendFileFuture.addListener(new ChannelProgressiveFutureListener() {
+            @Override
+            public void operationComplete(ChannelProgressiveFuture future) throws Exception {
+                System.out.println("Transfer complete.");
+            }
+
+            @Override
+            public void operationProgressed(ChannelProgressiveFuture future, long progress, long total) throws Exception {
+                if (total < 0) {
+                    System.err.println(future.channel() + " Transfer progress: " + progress);
+                } else {
+                    System.out.println(progress + " / " + total);
+                }
+            }
+        });
+        ChannelFuture lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        if (!HttpHeaderValues.KEEP_ALIVE.equals(req.headers().get(HttpHeaderNames.CONNECTION))) {
+            lastContentFuture.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    private static final String INSECURE_URI = "..";
+
+    private String sanitizeUri(String uri) {
+        try {
+            uri = URLDecoder.decode(uri, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            try {
+                uri = URLDecoder.decode(uri, CharsetUtil.ISO_8859_1.name());
+            } catch (UnsupportedEncodingException e1) {
+                throw new Error();
+            }
+        }
+        if (!uri.startsWith(url)) {
+            return null;
+        }
+        if (!uri.startsWith("/")) {
+            return null;
+        }
+        uri = uri.replace('/', File.separatorChar);
+        if (uri.contains(File.separator + '.') ||
+                uri.contains('.' + File.separator) ||
+                uri.startsWith(".") || uri.endsWith(".") ||
+                INSECURE_URI.equals(uri)) {
+            return null;
+        }
+        return System.getProperty("user.dir") + File.separator + uri;
+    }
+
+    private static final Pattern INSECURE_URI_PATTERN = Pattern.compile(".*[<>&\"].*");
+
+    private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
+        DefaultFullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1,
+                status,
+                Unpooled.copiedBuffer("Failure: " + status.toString() + "\r\n", CharsetUtil.UTF_8));
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private static void sendListing(ChannelHandlerContext ctx, File dir) {
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html; charset=UTF-8");
+        StringBuilder buf = new StringBuilder();
+        String dirPath = dir.getPath();
+        buf.append("<!DOCTYPE html>\r\n");
+        buf.append("<html><head><title>");
+        buf.append(dirPath);
+        buf.append(" 目录：");
+        buf.append("</title></head><body>\r\n");
+        buf.append("<h3>");
+        buf.append(dirPath).append(" 目录：");
+        buf.append("</h3>\r\n");
+        buf.append("<ul>");
+        buf.append("<li>链接：<a href=\"../\">..</a></li>\r\n");
+        for (File f : dir.listFiles()) {
+            if (f.isHidden() || !f.canRead()) {
+                continue;
+            }
+            String name = f.getName();
+            if (INSECURE_URI_PATTERN.matcher(name).matches()) {
+                continue;
+            }
+            buf.append("<li>链接：<a href=\"");
+            buf.append(name);
+            buf.append("\">");
+            buf.append(name);
+            buf.append("</a></li>\r\n");
+        }
+        buf.append("</ul></body></html>\r\n");
+        ByteBuf buffer = Unpooled.copiedBuffer(buf, CharsetUtil.UTF_8);
+        response.content().writeBytes(buffer);
+        buffer.release();
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private static void sendRedirect(ChannelHandlerContext ctx, String newUri) {
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1, HttpResponseStatus.FOUND);
+        response.headers().set(HttpHeaderNames.LOCATION, newUri);
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
 }
